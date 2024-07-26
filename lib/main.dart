ad5f8e99caf0facea5e51ad3c14861717cc6056f
karthik-kata <s.karthik.kata@gmail.com>
2024-07-26 01:03:12 -0400

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,10 @@ class SandGame extends FlameGame with HasCollisionDetection{
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-   
 
     add(ScreenHitbox());
 
-    for(int i = 0; i<=2;i++){
+    for(int i = 0; i<1;i++){
       add(Sand());
     }
    
@@ -92,13 +91,14 @@ class Sand extends SpriteComponent with HasGameReference<SandGame>, DragCallback
     
   }
 
+
   @override
   void onCollisionStart(Set<Vector2> intersectionPoints, PositionComponent other){
     super.onCollisionStart(intersectionPoints, other);
 
     if (other is Sand && (!hitBy.contains(other) || !other.hitBy.contains(this)) ){
       other.hitBy.add(this);
-      hitBy.add(this);
+      hitBy.add(other);
 
       var velocitySum = velocity + other.velocity;
       velocitySum = velocitySum * 0.5;
@@ -163,6 +163,7 @@ class Sand extends SpriteComponent with HasGameReference<SandGame>, DragCallback
 
     if(other is Sand && (hitBy.contains(other) || other.hitBy.contains(this))){
       hitBy.remove(other);
+      other.hitBy.remove(this);
     }
 
 
@@ -171,24 +172,16 @@ class Sand extends SpriteComponent with HasGameReference<SandGame>, DragCallback
 
 
   
-  //static const gravity = Velocity(pixelsPerSecond: Offset(0, 20));
 
-  static const gravity = Offset(0, 500);
 
   @override
   void update(double dt){
     super.update(dt);
     
-//    position.add(velocity.pixelsPerSecond.toVector2().scaled(dt));
-
-    var positionx = velocity.pixelsPerSecond.toVector2().scaled(dt).x;
-    var positiony = velocity.pixelsPerSecond.toVector2().scaled(dt).y;
-
-    position.add(Vector2(positionx, positiony));
+    position.add(velocity.pixelsPerSecond.toVector2().scaled(dt));
 
     position.clamp(Vector2(0, 0), Vector2(game.size.x - this.size.x , game.size.y - this.size.y));
 
-    //DO IT FOR CORNERS
     try{
     for(PositionComponent other in activeCollisions){
       if(other is Sand){
@@ -202,13 +195,13 @@ class Sand extends SpriteComponent with HasGameReference<SandGame>, DragCallback
       }
       else if(diffOfX.abs() > diffOfY.abs() && diffOfX.sign == 1){
         //this object x > other object x, therefore LEFT OF OTHER
-        position.clamp(Vector2(0, 0 ), Vector2(other.position.x, game.size.y - this.size.y));
+        position.clamp(Vector2(0, 0 ), Vector2(other.position.x - other.size.x, game.size.y - this.size.y));
         other.position.clamp(Vector2(this.position.x + this.size.x, 0 ), Vector2(game.size.x - other.size.x , game.size.y - other.size.y));
 
       }
       else if(diffOfX.abs() < diffOfY.abs() && diffOfY.sign == -1){
         //this object y > other object y, therefore BELOW OTHER
-        position.clamp(Vector2(0, other.position.y ), Vector2(game.size.x - this.size.x, game.size.y - this.size.y));
+        position.clamp(Vector2(0, other.position.y+other.size.y ), Vector2(game.size.x - this.size.x, game.size.y - this.size.y));
         other.position.clamp(Vector2(0, 0 ), Vector2(game.size.x - other.size.x, this.position.y));
 
 
@@ -225,53 +218,17 @@ class Sand extends SpriteComponent with HasGameReference<SandGame>, DragCallback
       }
     }
     }
-    catch(exceptions){
-      print(exceptions);
-    }
+    catch(exceptions){}
     
-    //Drag
-    const stopOffset = 0.001;
-    var x = velocity.pixelsPerSecond.dx + stopOffset;
-    var y = velocity.pixelsPerSecond.dy + stopOffset;
+ 
 
-    x = x*x * x.sign * -1 * 0.0001;
-    y = y*y * y.sign * -1 * 0.0001;
-
-
-    //var dragVelocity = Velocity(pixelsPerSecond: Offset(x,y));
-
-    var dragAccel = Offset(x, y)*50;
-
-    if((x.abs() <= 0.005 ) && (y.abs() <= 0.005)) {
-      //velocity = Velocity.zero;
-    }
-    else{
-      //velocity +=  Velocity(pixelsPerSecond: dragAccel*dt);
-    }
-  
     //Gravity
+    //  static const gravity = Offset(0, 500);
     //velocity += Velocity(pixelsPerSecond: gravity*dt);
 
 
 
-    /*
-    //Return to playing field
   
-      if (position.x <= -game.size.x * 0.1) {
-      // Left wall (or one of the leftmost corners)
-      velocity += const Velocity(pixelsPerSecond: Offset(150, 0)) ;
-    } else if (position.y <= -game.size.y * 0.1) {
-      // Top wall (or one of the upper corners)
-      velocity += const Velocity(pixelsPerSecond: Offset(0, 150));
-    } else if (position.x >= game.size.x*1.1) {
-      // Right wall (or one of the rightmost corners)
-      velocity += const Velocity(pixelsPerSecond: Offset(-150, 0));
-    } else if (position.y >= game.size.y * 1.1) {
-      // Bottom wall (or one of the bottom corners)
-      velocity += const Velocity(pixelsPerSecond: Offset(0, -150));
-    }   
-  */
-
 
 
   }
